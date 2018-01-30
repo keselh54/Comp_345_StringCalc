9e58960f640b5cb627a99029c0f0addecdb3a39c
keselh54 <hkesel@ithaca.edu>
2018-01-30 14:48:43 -0500

    Re adding my StringCalculatorImpl.java

--- src/edu/ithaca/kesel/StringCalculatorImpl.java
+++ src/edu/ithaca/kesel/StringCalculatorImpl.java
@@ -4,9 +4,29 @@ public class StringCalculatorImpl implements StringCalculator {
 
     @Override
     public int add(String stringOfNumbers){
-        if(stringOfNumbers == ""){
+
+        //If there are no items in the string
+        if(stringOfNumbers == null || stringOfNumbers.isEmpty()){
             return 0;
         }
+
+
+
+        //If there is 1 item in the string.
+        if(stringOfNumbers.matches("-?\\d?\\d?\\d?\\d")){
+
+            String[] stringArray = stringOfNumbers.split(",");
+
+            return Integer.valueOf(stringArray[0]);
+        }
+
+
+
+
+
+
+
+
         return -1;
     }
 
