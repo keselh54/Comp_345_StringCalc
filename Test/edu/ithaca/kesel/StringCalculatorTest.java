8553c57c28c40695df7aa082639f4980abd1ddd9
keselh54 <hkesel@ithaca.edu>
2018-01-30 12:19:39 -0500

    added the file: StringCalculatorTest.java

--- Test/edu/ithaca/kesel/StringCalculatorTest.java
+++ Test/edu/ithaca/kesel/StringCalculatorTest.java
@@ -1,7 +1,26 @@
 package edu.ithaca.kesel;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
+
+    StringCalculator myFunc = new StringCalculatorImpl();
+
+    @org.junit.jupiter.api.Test
+    void testStringCalculator(){
+
+    //First Test Case: If no values are in the string.
+        //Create an empty string.
+
+        String emptyString = "";
+
+        int testInt = myFunc.add(emptyString);
+
+        assertEquals(0, testInt);
+
+    }
 
 }
